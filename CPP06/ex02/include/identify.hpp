730ae73ff23dc0f1ad745a1878e04c1681667175
purple <purple.pro@gmail.com>
2023-11-15 14:33:47 +0100

    06

---
+++ CPP06/ex02/include/identify.hpp
@@ -0,0 +1,42 @@
+#pragma once
+
+#include <iostream>
+#include <string>
+
+
+class Base{
+	public		:
+					virtual ~Base();
+
+	protected	:
+
+	private		:
+
+};
+
+class A : public Base{
+	
+	public		:
+
+	protected	:
+
+	private		:
+};
+
+class B : public Base{
+	
+	public		:
+
+	protected	:
+
+	private		:
+};
+
+class C : public Base{
+	
+	public		:
+
+	protected	:
+
+	private		:
+};
